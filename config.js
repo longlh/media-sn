8e8c554a02640cb171d148b5320e32833c12fe04
LongLH <d@dapps.me>
2017-07-08 00:47:46 +0700

    sample configuration

--- config.js
+++ config.js
@@ -1,2 +1,22 @@
 module.exports = {
+	dev: {
+		url: '127.0.0.1:3000',
+		server: {
+			port: 3000
+		},
+		redis: {
+			port: 6379,
+			host: '127.0.0.1'
+		},
+		db: {
+			url: 'mongodb://127.0.0.1/image-feed'
+		},
+		s3: {
+			accessKeyId: 'xxx',
+			secretAccessKey: 'yyy',
+			bucket: 'zzz',
+			region: 'us-east-1'
+		},
+		theme: 'default'
+	}
 };
