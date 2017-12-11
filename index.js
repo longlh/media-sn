265140ef3569c25aa5189e227a579fd077e66906
LongLH <d@dapps.me>
2017-12-11 16:42:50 +0700

    update

--- index.js
+++ index.js
@@ -2,3 +2,11 @@
 // const port = system.get('config').server.port;
 
 // system.listen(port, () => console.log('Started at :%d', port));
+
+import recursiveConfig from 'recursive-config'
+
+const config = recursiveConfig.load({
+  defaults: {}
+})
+
+console.log(config)
