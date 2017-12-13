20d3168db04a97e27065f32fa2eb623fd3a6fc93
LongLH <d@dapps.me>
2017-12-13 10:23:12 +0700

    update config.example.js

--- config.example.js
+++ config.example.js
@@ -1,32 +1,34 @@
+const path = require('path')
+
 module.exports = {
-	dev: {
-		url: 'http://yourdomain.com',
-		server: {
-			port: 3000
-		},
-		redis: {
-			port: 6379,
-			host: '127.0.0.1'
-		},
-		db: {
-			url: 'mongodb://127.0.0.1/media-feed'
-		},
-		s3: {
-			accessKeyId: 'xxx',
-			secretAccessKey: 'yyy',
-			bucket: 'zzz',
-			region: 'us-east-1',
-			cname: 'yourcdndomain.net'
-		},
-		theme: 'default',
-		oauth: {
-			gg: {
-				clientId: 'xxx',
-				clientSecret: 'yyy',
-			}
-		},
-		debug: false,
-		admins: ['admin1@yourdomain.com', 'admin2@yourdomain.com'],
-		pageSize: 30
-	}
-};
+  url: 'http://yourdomain.com',
+  server: {
+    port: 3000
+  },
+  redis: {
+    port: 6379,
+    host: '127.0.0.1'
+  },
+  db: {
+    url: 'mongodb://127.0.0.1/media-feed'
+  },
+  s3: {
+    accessKeyId: 'xxx',
+    secretAccessKey: 'yyy',
+    bucket: 'zzz',
+    region: 'us-east-1',
+    cname: 'yourcdndomain.net'
+  },
+  theme: 'default',
+  oauth: {
+    gg: {
+      clientId: 'xxx',
+      clientSecret: 'yyy',
+    }
+  },
+  debug: true,
+  production: false,
+  admins: ['admin1@yourdomain.com', 'admin2@yourdomain.com'],
+  pageSize: 30,
+  uploadDir: path.resolve(__dirname, 'content/upload')
+}
