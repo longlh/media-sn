daa1be886cb73a8d2938d44716a4e871130e571d
LongLH <d@dapps.me>
2019-04-22 14:11:07 +0700

    handle multiple parts upload

--- src/core/api/index.js
+++ src/core/api/index.js
@@ -1,7 +1,57 @@
 import express from 'express'
+import formidable from 'formidable'
+import fs from 'fs-extra'
+import path from 'path'
+
+import config from '@core/infrastructure/config'
+
+const buffers = {}
 
 export default async () => {
   const app = express()
+
+  app.post('/upload', (req, res, next) => {
+    const form = new formidable.IncomingForm()
+
+    form.parse(req, (err, fields, files) => {
+      if (err) {
+        return next(err)
+      }
+
+      const storeDir = path.resolve(config.cwd, '../content/upload')
+
+      fs.ensureDirSync(storeDir)
+
+      const basename = fields.name.toLowerCase()
+      const tmpPath = files.file.path
+      const storePath = path.join(storeDir, fields.name)
+
+      const name = fields.name
+      const chunk = parseInt(fields.chunk, 10)
+      const chunks = parseInt(fields.chunks, 10)
+
+      const rs = fs.createReadStream(tmpPath)
+
+      rs.on('data', (chunk) => {
+        fs.writeFile(storePath, chunk, { flag: 'a' })
+      })
+
+      rs.on('end', () => {
+        fs.removeSync(tmpPath)
+
+        if (chunk < chunks - 1) {
+          return res.sendStatus(200)
+        }
+
+        res.sendStatus(201)
+      })
+    })
+  },
+  (err, req, res, next) => {
+    console.log(err)
+
+    next()
+  })
 
   return app
 }
--- src/themes/default/pages/personal/script/index.js
+++ src/themes/default/pages/personal/script/index.js
@@ -1,8 +1,41 @@
 import { Uploader } from 'plupload'
 
 const uploader = new Uploader({
-  browse_button: 'upload',
-  url: '/api/v1/upload'
+  browse_button: 'browse',
+  url: '/api/v1/upload',
+  chunk_size: '200kb',
+  unique_names: true
+})
+
+uploader.bind('FilesAdded', (uploader, files) => {
+  const queue = document.getElementById('queue')
+
+  files.forEach((file) => {
+     const item = document.createElement('li')
+
+     item.id = file.id
+     item.innerHTML = file.name
+
+     queue.appendChild(item)
+  })
+})
+
+uploader.bind('QueueChanged', (uploader) => {
+  const uploadButton = document.getElementById('upload')
+
+  uploadButton.style.visibility = uploader.files.length ? 'visible': 'hidden'
+})
+
+uploader.bind('FileUploaded', (uploader, file, result) => {
+  console.log(file, result)
+})
+
+uploader.bind('Error', (uploader, error) => {
+  console.error(error)
+})
+
+document.getElementById('upload').addEventListener('click', () => {
+  uploader.start()
 })
 
 uploader.init()
